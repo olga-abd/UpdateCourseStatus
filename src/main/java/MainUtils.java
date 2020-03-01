13f099c9e87edc60d77f4a688f1a0c230611ad8a
OLGA <o_lk_a@mail.ru>
2020-03-01 21:25:37 +0500

    Добавлены коментарии

--- src/main/java/MainUtils.java
+++ src/main/java/MainUtils.java
@@ -8,18 +8,23 @@ import java.util.Set;
 public class MainUtils {
     public static void run (){
         CourseDAO courseDAO = new CourseDAO();
+        // получаем список курсов
         List<Course> courses = courseDAO.getAllCourses();
 
+        // текущая дата
         Date currentDate = new Date(System.currentTimeMillis());
 
         for (Course course : courses){
+            // даты начала/окончания курса
             Date startDate = course.getDateStart();
             Date endDate = course.getDateEnd();
 
+            // если текущая дата между
             if (startDate.toLocalDate().compareTo(currentDate.toLocalDate()) <= 0 &&
                     endDate.toLocalDate().compareTo(currentDate.toLocalDate()) >= 0) {
                 Set<EmployeeCourse> employeeCourses = course.getEmployeeCourses();
 
+                // то для всех записанных на курс меняем статус на INPROGRESS, если статус другой
                 for (EmployeeCourse employeeCourse : employeeCourses) {
                     if (employeeCourse.getStatus() != CourseStatus.INPROGRESS) {
                         employeeCourse.setStatus(CourseStatus.INPROGRESS);
@@ -28,8 +33,11 @@ public class MainUtils {
                     }
                 }
             }
+            // если текущая дата после окончания курса
             else if (endDate.toLocalDate().compareTo(currentDate.toLocalDate()) < 0) {
                 Set<EmployeeCourse> employeeCourses = course.getEmployeeCourses();
+
+                // для всех записанных на курс меняем статус на COMPLETED, если статус другой
                 for (EmployeeCourse employeeCourse : employeeCourses) {
                     if (employeeCourse.getStatus() != CourseStatus.COMPLETED) {
                         employeeCourse.setStatus(CourseStatus.COMPLETED);
